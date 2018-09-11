2f1e0cd95e9a47de96bc07bbc0a54b25fa031a39
amweiss118 <43076621+amweiss118@users.noreply.github.com>
2018-09-11 16:58:44 -0400

    Add files via upload

--- c20gTraining/jquery.js
+++ c20gTraining/jquery.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
 	
-
-var el = $('#submit');
-   
-   el.click(function(e) {
-
-			e.preventDefault();
-
-			return false;
-
+	$(function () {
+	  $(document).scroll(function () {
+	    var $nav = $(".navbar-fixed-top");
+	    $nav.toggleClass('scrolled', $(this).scrollTop() > 600);
+	  });
 	});
+
+	var el = $('.icons');
+   
+    el.click(function(e) {
+
+ 			e.preventDefault();
+
+ 			
+
+ 			return false;
+
+ 	});
 	
 })
